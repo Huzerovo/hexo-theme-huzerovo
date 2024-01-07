5971671d31fd8b10a4d4a37e4a89607615d39372
Huzerovo <hulingoao@gmail.com>
2024-01-08 00:54:33 +0800

    feat: 增加知识共享授权信息

---
+++ scripts/get_cc.js
@@ -0,0 +1,12 @@
+// 获取版权知识共享说明
+
+hexo.extend.helper.register('get_cc', function() {
+    let cc = this.theme.creativecommons;
+    if (cc) {
+        let link = '<a href="' + cc.link + '">' 
+            + cc.listen 
+            + '</a>';
+        return cc.description.replaceAll('#', link);
+    }
+    return '';
+});
--- scripts/get_post_covers.js
+++ scripts/get_post_covers.js
@@ -6,21 +6,16 @@ hexo.extend.helper.register('get_post_covers', function(post) {
     // 文章自定义封面
     if (post.photos) {
         for (photo of post.photos) {
-            // 判断使用：相册集、文章资源目录或者网络图片
+            // 判断使用：本地或者网络图片
             let is_http_or_https = new RegExp(/^(http|https):\/\/[^\s]*/gm).test(photo);
             if (is_http_or_https) {
                 covers.push(photo);
             } else {
                 if (photo[0] !== '/') {
-                    if (post.gallery && post.gallery !== '') {
-                        // 使用相册集图片
-                        covers.push(this.url_for(post.gallery) + '/' + photo);
-                    } else {
-                        // 使用文章资源文件夹下的图片
-                        covers.push(this.url_for(post.path) + photo);
-                    }
+                    // 使用资源文件夹里的图片
+                    covers.push(this.url_for(post.path) + photo);
                 } else {
-                    // 使用绝对路径上的文件
+                    // 使用绝对路径上的图片
                     covers.push(this.url_for(photo));
                 }
             }
